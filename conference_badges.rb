126a64212dbb45b956bc30e8f6636fcefceb3720
Katrina Pritchard <pritckat@gmail.com>
2018-08-14 20:27:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.each_with_index { |name, index|
+  %warray.each_with_index { |name, index|
     "Hello, #{name}! You'll be assigned to room #{index}!"
   }
 end
